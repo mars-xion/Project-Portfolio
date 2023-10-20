ff0e33410b69f96d131f9b872dc1d7775b30ad95
mars-xion <maz200027@gmail.com>
2023-10-20 17:39:01 +0200

    trying to display each project on each LA

--- 3D_porto/src/components/LAe.jsx
+++ 3D_porto/src/components/LAe.jsx
@@ -6,20 +6,77 @@ import {
     TabPanel,
   } from "@material-tailwind/react";
 
-import { LAData } from "../constants";
+import { LAData, LAprojects } from "../constants";
 import { SectionWrapper } from "../hoc";
 import React, { useState } from "react";
 
-  
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+import { styles } from '../styles';
+import { fadeIn, textVariant } from "../utils/motion";
+import { github } from '../assets';
+
+
+const ProjectCard = ({ index, name, description,
+  tags, image, source_code_link}) => {
+   return(
+     <motion.div 
+       variants={fadeIn("up", "spring", index * 0.5, 0.75)}> 
+     <Tilt 
+     options={{
+       max: 45,
+       scale: 1,
+       speed: 450
+     }}
+     className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full">
+       <div className="relative w-full h-[230px]">
+         <img src={image} alt={name}
+         className="w-full h-full object-cover rounded-2xl" />
+         <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+           <div onClick={() => window.open (source_code_link, "_blank")}
+             className="black-gradient w-10 h-10 rounded-full
+             flex justify-center items-center cursor-pointer"
+           >
+             <img 
+             src={github}
+             alt="github"
+             className="w-1/2 h-1/2 object-contain"/>
+
+           </div>
+         </div>
+       </div>
+       <div className="mt-5">
+         <h3 className="text-white font-bold text-[24px]">{name}</h3>
+         <p className="mt-2 text-secondary text-[14px]">{description}</p>
+       </div>
+       <div className="mt-4 flex flex-wrap gap-2">
+         {tags.map((tag) => (
+           <p key={tag.name} className={`text-[14px] ${tag.color}`}>
+             #{tag.name}
+           </p>
+         ))}
+       </div>
+     </Tilt>
+     </ motion.div>
+   )
+}
+
+
 const LAe = () => {
     const data = LAData.jobs ;
     // Set initial selected tab
     const [selectedTab, setSelectedTab] = useState(data[0].expData.position); 
-
     
     const handleTabChange = (value) => {
       setSelectedTab(value);
     };
+
+    // filter project depininding on the type of the Learning Agreements
+    const filteredProjects = LAprojects.filter(
+      (LAprojects) => LAprojects.position === selectedTab
+    );
+    
+    //console.log(filteredProjects.map((LAprojects) => LAprojects.position));
 
     return (
       <Tabs value={selectedTab} onChange={handleTabChange} 
@@ -49,7 +106,7 @@ const LAe = () => {
              }}
              onClick={() => handleTabChange(expData.position)}
            >
-              {expData.company}
+              {expData.title}
             </Tab>
           ))}
           <span className={`border-r-2 border-purple-500 h-auto absolute top-4 bottom-8 right-4 m-auto`} />
@@ -58,11 +115,25 @@ const LAe = () => {
         <TabsBody>
           {data.map(({ expData }) => (
             <TabPanel key={expData.position} value={expData.position} className="py-0">
-              <h3 className="">{expData.position}</h3>
+              <h2>{expData.position}</h2>
+              <h3>{expData.period}</h3>
               <br />
-              <p>{expData.details}</p>
+              <p>{expData.details.join("\n")}</p>
+
+              {filteredProjects.length > 0 ? (
+                <div className="mt-20 flex flex-wrap gap-7">
+                  {filteredProjects.map((LAprojects, index) => (
+                    <ProjectCard key={`LAprojects-${index}`} index={index} {...LAprojects} />
+
+                  ))}
+                </div>
+              ) : (
+                <p>No projects found for the selected position.</p>
+              )}
+
             </TabPanel>
           ))}
+              
         </TabsBody>
         </div>
       </Tabs>
--- 3D_porto/src/constants/index.js
+++ 3D_porto/src/constants/index.js
@@ -260,8 +260,8 @@ import {
     jobs: [
       {
         expData: {
-          company: "All",
-          position: "Graduate Student",
+          title: "All",
+          position: "All Projects",
           period: "Oct 2018 - present",
           details: [
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc dignissim fringilla dui ac mattis.",
@@ -271,40 +271,149 @@ import {
       },
       {
         expData: {
-          company: "LA1",
-          position: "Research Assistant",
+          title: "LA1",
+          position: "User interaction @analysis @advice",
           period: "Oct 2016 - May 2018",
           details: [
-            "Suspendisse potenti. Vestibulum aliquam luctus sem, at feugiat felis. Pellentesque dignissim lorem eu ipsum condimentum varius. ",
-            "Nam vehicula pretium arcu. Nam venenatis ante et porta pellentesque."
+            "Analyse the user, the interaction, and the user experience, also taking state of the art interactive technologies into account. ",
+            "Select a suitable design process to be able to advise on UX interventions based on a validated UX design."
           ]
         }
       },
       {
         expData: {
-          company: "LA2",
-          position: "Graduate Student",
+          title: "LA2",
+          position: "User interaction @execution @validation)",
           period: "Oct 2018 - present",
           details: [
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc dignissim fringilla dui ac mattis.",
-            "Donec in sodales eros. Nulla fermentum, ante in venenatis pellentesque, justo odio viverra lorem, varius posuere erat tortor et magna."
+            "Execute and evaluate the user experience of an interactive product.",
+            "Document the development process for the stakeholders."
           ]
         }
       },
       {
         expData: {
-          company: "LA3",
-          position: "Product Development Engineer",
+          title: "LA3",
+          position: "Software design and realisation",
           period: "Mar 2016 - Oct 2016",
           details: [
-            "Ut lobortis neque sit amet felis aliquam, sit amet placerat leo maximus. Aenean a tempus massa, vel varius leo.",
-            "Sed eu efficitur leo, nec congue elit. Etiam ullamcorper est at pulvinar condimentum.",
-            "Suspendisse potenti. Vestibulum aliquam luctus sem, at feugiat felis. Pellentesque dignissim lorem eu ipsum condimentum varius. "
+            "Create & design software with existing components or libraries using predetermined quality criteria and version control. "
+          ]
+        }
+      },
+      {
+        expData: {
+          title: "LA4",
+          position: "Future-oriented organisation",
+          period: "Mar 2016 - Oct 2016",
+          details: [
+            "Carry out a problem analysis and on that basis, determine the definitive problem and elaborate on this in a project plan."
+          ]
+        }
+      },
+      {
+        expData: {
+          title: "LA5",
+          position: "Investigative problem solving",
+          period: "Mar 2016 - Oct 2016",
+          details: [
+            "formulate sub-questions pertaining to the primary question and answer these using relevant research methods. ",
+            "Use the conclusions of the sub-questions to justify (design) choices."
+          ]
+        }
+      },
+      {
+        expData: {
+          title: "LA6",
+          position: "Personal leadership",
+          period: "Mar 2016 - Oct 2016",
+          details: [
+            "Methodically reflect on my professional identity and personal development.  "
+          ]
+        }
+      },
+      {
+        expData: {
+          title: "LA7",
+          position: "Goal-oriented interaction",
+          period: "Mar 2016 - Oct 2016",
+          details: [
+            "communicate with different stakeholders and team members about the ICT assignment"
           ]
         }
       }
     ]
   };
+
+  const LAprojects = [
+    {
+      name: "Car Rent",
+      position: "Goal-oriented interaction",
+      description:
+        "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: carrent,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Job IT",
+      position: "Investigative problem solving",
+      description:
+        "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "restapi",
+          color: "green-text-gradient",
+        },
+        {
+          name: "scss",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: jobit,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Trip Guide",
+      position: "User interaction @execution @validation)",
+      description:
+        "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+      tags: [
+        {
+          name: "nextjs",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "supabase",
+          color: "green-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: tripguide,
+      source_code_link: "https://github.com/",
+    },
+  ];
+
   
   
-export { services, technologies, experiences, testimonials, projects,  LAs, LAData};
+export { services, technologies, experiences, testimonials, projects,  LAs, LAData, LAprojects};
