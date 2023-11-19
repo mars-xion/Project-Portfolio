dc90683d13b104e116aa71a02f75e10d89088715
mars-xion <maz200027@gmail.com>
2023-11-19 17:40:11 +0100

    Nav menu, App.js, And styling :)

--- portov2/src/App.js
+++ portov2/src/App.js
@@ -1,12 +1,27 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Pages:
 import Nav from "./nav/Nav.js";
+import About from "./about/About";
+import Skills from "./skills/Skills";
+import Projects from "./projects/Projects";
+import Contact from "./contact/Contact";
+
+// Styles:
+import "./styles/app.css";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Nav />
-    </>
+      <Routes>
+        <Route path="/" element={About} />
+        <Route path="/skills" element={Skills} />
+        <Route path="/projects" element={Projects} />
+        <Route path="/contact" element={Contact} />
+      </Routes>
+    </Router>
   );
 };
 
