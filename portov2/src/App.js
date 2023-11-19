9657afbe26092e9711ffe30763457224ac579ab4
mars-xion <maz200027@gmail.com>
2023-11-19 22:10:38 +0100

    Fixed font error, added the playerstats and  imgs

--- portov2/src/App.js
+++ portov2/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-// components:
+// Components:
 import Nav from "./nav/Nav.js";
 import About from "./about/About";
 import Skills from "./skills/Skills";
 import Projects from "./projects/Projects";
 import Contact from "./contact/Contact";
 import Background from "./background/Background.js";
+import PlayerStats from "./playerStats/PlayerStats.js";
 
 // Styles:
 import "./styles/app.css";
@@ -23,6 +24,7 @@ const App = () => {
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+      <PlayerStats />
     </Router>
   );
 };
