82336ee547df30d4f5845896bd572b3ae937505d
mars-xion <maz200027@gmail.com>
2023-11-19 18:26:15 +0100

    background video

--- portov2/src/App.js
+++ portov2/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-// Pages:
+// components:
 import Nav from "./nav/Nav.js";
 import About from "./about/About";
 import Skills from "./skills/Skills";
 import Projects from "./projects/Projects";
 import Contact from "./contact/Contact";
+import Background from "./background/Background.js";
 
 // Styles:
 import "./styles/app.css";
@@ -15,11 +16,12 @@ const App = () => {
   return (
     <Router>
       <Nav />
+      <Background />
       <Routes>
-        <Route path="/" element={About} />
-        <Route path="/skills" element={Skills} />
-        <Route path="/projects" element={Projects} />
-        <Route path="/contact" element={Contact} />
+        <Route path="/" element={<About />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
