58fe832eebe3b36c593d98cf975fe0cfeb561e7e
mars-xion <maz200027@gmail.com>
2023-12-19 09:17:47 +0100

    alot haha

--- 3D_porto/src/components/LAe.jsx
+++ 3D_porto/src/components/LAe.jsx
@@ -77,113 +77,108 @@ const ProjectCard = ({
 };
 
 const LAe = () => {
-  const data = LAData.jobs;
-  // Set initial selected tab
-  const [selectedTab, setSelectedTab] = useState(data[0].expData.position);
-
-  // filter project depininding on the type of the Learning Agreements
-  let filteredProjects = [];
-  if (selectedTab === "All Projects") {
-    // Show all projects when the position is "All"
-    filteredProjects = LAprojects;
-  } else {
-    // Filter projects based on the selected position
-    filteredProjects = LAprojects.filter(
-      (LAproject) => LAproject.position === selectedTab
-    );
-  }
-
-  // Log the selected tab and filtered projects whenever the component renders
-  console.log("Selected Tab:", selectedTab);
-  console.log("Filtered Projects:", filteredProjects);
-
-  // Log state changes when the component mounts and whenever selectedTab changes
-  useEffect(() => {
-    console.log("Selected Tab:", selectedTab);
-  }, [selectedTab]); // useEffect will run whenever selectedTab change
-
-  const handleTabChange = (value) => {
-    console.log("Selected Tab (change):", value); // Log the selected tab value
-    // filteredProjects = LAprojects.filter((LAproject) => LAproject.position === value);
-    console.log("Filtered Projects (change):", filteredProjects); // Log the filtered projects
-    setSelectedTab(value);
-  };
-
-  return (
-    <Tabs
-      value={selectedTab}
-      onChange={handleTabChange}
-      orientation="vertical"
-      className="max-w-6xl"
-    >
-      <TabsHeader className="w-44 p-4">
-        {data.map(({ expData }) => (
-          <Tab
-            key={expData.position}
-            value={expData.position}
-            style={{
-              color: selectedTab === expData.position ? "#4B527E" : "white",
-              borderRight:
-                selectedTab === expData.position
-                  ? "4px solid #4B527E"
-                  : "2px solid transparent",
-              backgroundColor:
-                selectedTab === expData.position
-                  ? "rgba(128, 0, 128, 0.1)" // Background color with low opacity for the selected tab
-                  : "transparent",
-              paddingRight: "8px",
-              paddingLeft: "12px", // Adjust padding between text and border
-              marginBottom: "15px", // Adjust vertical spacing between tabs
-              fontSize: selectedTab === expData.position ? "20px" : "18px",
-              cursor: "pointer", // Show pointer cursor on hover
-              paddingTop: "8px", // Add padding within the background box
-              paddingBottom: "8px", // Add padding within the background box
-            }}
-            onClick={() => handleTabChange(expData.position)}
-          >
-            {expData.title}
-          </Tab>
-        ))}
-        <span
-          className={`border-r-2 border-purple-500 h-auto absolute top-4 bottom-8 right-4 m-auto`}
-        />
-      </TabsHeader>
-      <div className="p-4" key={selectedTab}>
-        <TabsBody>
-          {data.map(({ expData }) => (
-            <TabPanel
-              key={expData.position}
-              value={expData.position}
-              className="py-0"
-            >
-              <h2>{expData.position}</h2>
-              <h3>{expData.period}</h3>
-              <br />
-              <p>{expData.details.join("\n")}</p>
-              {filteredProjects.length > 0 ? (
-                <div className="mt-20 flex flex-wrap gap-7">
-                  {filteredProjects.map((LAprojects, index) => (
-                    <div key={`project-${index}`}>
-                      {/* Applicant name:  {ProjectCard.name} */}
-                      <ProjectCard index={index} {...LAprojects} />
-                    </div>
-                  ))}
-                </div>
-              ) : (
-                <>
-                  <br />
-                  <br />
-                  <p>
-                    No related projects yet for the selected Learning agreement.
-                  </p>
-                </>
-              )}
-            </TabPanel>
-          ))}
-        </TabsBody>
-      </div>
-    </Tabs>
-  );
+  // const data = LAData.jobs;
+  // // Set initial selected tab
+  // const [selectedTab, setSelectedTab] = useState(data[0].expData.position);
+  // // filter project depininding on the type of the Learning Agreements
+  // let filteredProjects = [];
+  // if (selectedTab === "All Projects") {
+  //   // Show all projects when the position is "All"
+  //   filteredProjects = LAprojects;
+  // } else {
+  //   // Filter projects based on the selected position
+  //   filteredProjects = LAprojects.filter(
+  //     (LAproject) => LAproject.position === selectedTab
+  //   );
+  // }
+  // // Log the selected tab and filtered projects whenever the component renders
+  // console.log("Selected Tab:", selectedTab);
+  // console.log("Filtered Projects:", filteredProjects);
+  // // Log state changes when the component mounts and whenever selectedTab changes
+  // useEffect(() => {
+  //   console.log("Selected Tab:", selectedTab);
+  // }, [selectedTab]); // useEffect will run whenever selectedTab change
+  // const handleTabChange = (value) => {
+  //   console.log("Selected Tab (change):", value); // Log the selected tab value
+  //   // filteredProjects = LAprojects.filter((LAproject) => LAproject.position === value);
+  //   console.log("Filtered Projects (change):", filteredProjects); // Log the filtered projects
+  //   setSelectedTab(value);
+  // };
+  // return (
+  //   <Tabs
+  //     value={selectedTab}
+  //     onChange={handleTabChange}
+  //     orientation="vertical"
+  //     className="max-w-6xl"
+  //   >
+  //     <TabsHeader className="w-44 p-4">
+  //       {data.map(({ expData }) => (
+  //         <Tab
+  //           key={expData.position}
+  //           value={expData.position}
+  //           style={{
+  //             color: selectedTab === expData.position ? "#4B527E" : "white",
+  //             borderRight:
+  //               selectedTab === expData.position
+  //                 ? "4px solid #4B527E"
+  //                 : "2px solid transparent",
+  //             backgroundColor:
+  //               selectedTab === expData.position
+  //                 ? "rgba(128, 0, 128, 0.1)" // Background color with low opacity for the selected tab
+  //                 : "transparent",
+  //             paddingRight: "8px",
+  //             paddingLeft: "12px", // Adjust padding between text and border
+  //             marginBottom: "15px", // Adjust vertical spacing between tabs
+  //             fontSize: selectedTab === expData.position ? "20px" : "18px",
+  //             cursor: "pointer", // Show pointer cursor on hover
+  //             paddingTop: "8px", // Add padding within the background box
+  //             paddingBottom: "8px", // Add padding within the background box
+  //           }}
+  //           onClick={() => handleTabChange(expData.position)}
+  //         >
+  //           {expData.title}
+  //         </Tab>
+  //       ))}
+  //       <span
+  //         className={`border-r-2 border-purple-500 h-auto absolute top-4 bottom-8 right-4 m-auto`}
+  //       />
+  //     </TabsHeader>
+  //     <div className="p-4" key={selectedTab}>
+  //       <TabsBody>
+  //         {data.map(({ expData }) => (
+  //           <TabPanel
+  //             key={expData.position}
+  //             value={expData.position}
+  //             className="py-0"
+  //           >
+  //             <h2>{expData.position}</h2>
+  //             <h3>{expData.period}</h3>
+  //             <br />
+  //             <p>{expData.details.join("\n")}</p>
+  //             {filteredProjects.length > 0 ? (
+  //               <div className="mt-20 flex flex-wrap gap-7">
+  //                 {filteredProjects.map((LAprojects, index) => (
+  //                   <div key={`project-${index}`}>
+  //                     {/* Applicant name:  {ProjectCard.name} */}
+  //                     <ProjectCard index={index} {...LAprojects} />
+  //                   </div>
+  //                 ))}
+  //               </div>
+  //             ) : (
+  //               <>
+  //                 <br />
+  //                 <br />
+  //                 <p>
+  //                   No related projects yet for the selected Learning agreement.
+  //                 </p>
+  //               </>
+  //             )}
+  //           </TabPanel>
+  //         ))}
+  //       </TabsBody>
+  //     </div>
+  //   </Tabs>
+  // );
 };
 
 export default SectionWrapper(LAe, "");
---
+++ ToBeFinalVersion/src/constants/index.js
@@ -0,0 +1,419 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    starbucks,
+    tesla,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "React Native Developer",
+      icon: mobile,
+    },
+    {
+      title: "Backend Developer",
+      icon: backend,
+    },
+    {
+      title: "Content Creator",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "figma",
+      icon: figma,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "React.js Developer",
+      company_name: "Starbucks",
+      icon: starbucks,
+      iconBg: "#383E56",
+      date: "March 2020 - April 2021",
+      points: [
+        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+    {
+      title: "React Native Developer",
+      company_name: "Tesla",
+      icon: tesla,
+      iconBg: "#E6DEDD",
+      date: "Jan 2021 - Feb 2022",
+      points: [
+        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+    {
+      title: "Web Developer",
+      company_name: "Shopify",
+      icon: shopify,
+      iconBg: "#383E56",
+      date: "Jan 2022 - Jan 2023",
+      points: [
+        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+    {
+      title: "Full stack Developer",
+      company_name: "Meta",
+      icon: meta,
+      iconBg: "#E6DEDD",
+      date: "Jan 2023 - Present",
+      points: [
+        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+      name: "Sara Lee",
+      designation: "CFO",
+      company: "Acme Co",
+      image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+      testimonial:
+        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+      name: "Chris Brown",
+      designation: "COO",
+      company: "DEF Corp",
+      image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      testimonial:
+        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Lisa Wang",
+      designation: "CTO",
+      company: "456 Enterprises",
+      image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Car Rent",
+      description:
+        "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: carrent,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Job IT",
+      description:
+        "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "restapi",
+          color: "green-text-gradient",
+        },
+        {
+          name: "scss",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: jobit,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Trip Guide",
+      description:
+        "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+      tags: [
+        {
+          name: "nextjs",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "supabase",
+          color: "green-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: tripguide,
+      source_code_link: "https://github.com/",
+    },
+  ];
+
+  const LAs = [
+    {
+      name: "react course",
+      description:
+        "Web-based",
+      
+    },
+    {
+      name: "CV IT",
+      description:
+        "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+    }
+  ];
+
+  const LAData = {
+    jobs: [
+      {
+        expData: {
+          title: "All",
+          position: "All Projects",
+          period: "Oct 2018 - present",
+          details: [
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc dignissim fringilla dui ac mattis.",
+            "Donec in sodales eros. Nulla fermentum, ante in venenatis pellentesque, justo odio viverra lorem, varius posuere erat tortor et magna."
+          ]
+        }
+      },
+      {
+        expData: {
+          title: "LA1",
+          position: "User interaction @analysis @advice",
+          period: "Oct 2016 - May 2018",
+          details: [
+            "Analyse the user, the interaction, and the user experience, also taking state of the art interactive technologies into account. ",
+            "Select a suitable design process to be able to advise on UX interventions based on a validated UX design."
+          ]
+        }
+      },
+      {
+        expData: {
+          title: "LA2",
+          position: "User interaction @execution @validation)",
+          period: "Oct 2018 - present",
+          details: [
+            "Execute and evaluate the user experience of an interactive product.",
+            "Document the development process for the stakeholders."
+          ]
+        }
+      },
+      {
+        expData: {
+          title: "LA3",
+          position: "Software design and realisation",
+          period: "Mar 2016 - Oct 2016",
+          details: [
+            "Create & design software with existing components or libraries using predetermined quality criteria and version control. "
+          ]
+        }
+      },
+      {
+        expData: {
+          title: "LA4",
+          position: "Future-oriented organisation",
+          period: "Mar 2016 - Oct 2016",
+          details: [
+            "Carry out a problem analysis and on that basis, determine the definitive problem and elaborate on this in a project plan."
+          ]
+        }
+      },
+      {
+        expData: {
+          title: "LA5",
+          position: "Investigative problem solving",
+          period: "Mar 2016 - Oct 2016",
+          details: [
+            "formulate sub-questions pertaining to the primary question and answer these using relevant research methods. ",
+            "Use the conclusions of the sub-questions to justify (design) choices."
+          ]
+        }
+      },
+      {
+        expData: {
+          title: "LA6",
+          position: "Personal leadership",
+          period: "Mar 2016 - Oct 2016",
+          details: [
+            "Methodically reflect on my professional identity and personal development.  "
+          ]
+        }
+      },
+      {
+        expData: {
+          title: "LA7",
+          position: "Goal-oriented interaction",
+          period: "Mar 2016 - Oct 2016",
+          details: [
+            "communicate with different stakeholders and team members about the ICT assignment"
+          ]
+        }
+      }
+    ]
+  };
+
+  const LAprojects = [
+    {
+      name: "Car Rent",
+      position: "Goal-oriented interaction",
+      description:
+        "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: carrent,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Job IT",
+      position: "Investigative problem solving",
+      description:
+        "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "restapi",
+          color: "green-text-gradient",
+        },
+        {
+          name: "scss",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: jobit,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Trip Guide",
+      position: "User interaction @execution @validation)",
+      description:
+        "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+      tags: [
+        {
+          name: "nextjs",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "supabase",
+          color: "green-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: tripguide,
+      source_code_link: "https://github.com/",
+    },
+  ];
+
+  
+  
+export { services, technologies, experiences, testimonials, projects,  LAs, LAData, LAprojects};
